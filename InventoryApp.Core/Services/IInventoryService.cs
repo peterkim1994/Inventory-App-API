50917bd4496636d5ef3bcc86334a1cf8428b4aed
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2021-05-01 16:18:00 +1200

    hello

---
+++ InventoryApp.Core/Services/IInventoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InventoryPOS.DataStore.Models;
+
+namespace InventoryPOSApp.Core.Services
+{
+    public interface IInventoryService
+    {
+        Colour AddColour(Colour colour);
+    }
+}
