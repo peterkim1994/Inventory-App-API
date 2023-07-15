68fdeabfe7cf6d26452c904322986831a20418d5
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2023-07-15 16:44:30 +1200

    add store fk to products

--- Inventory.DataStore/Daos/Product.cs
+++ Inventory.DataStore/Daos/Product.cs
@@ -11,28 +11,43 @@ namespace InventoryPOS.DataStore.Daos
     {
         [Key]
         public int Id { get; set; }
+
         public long Barcode { get; set; }
+
         [MaxLength(50)]
         public string ManufactureCode { get; set; }
+
         [Required]
         [MinLength(5),MaxLength(100)]
         public string Description { get; set; }
+
         [Required]
         public int BrandId { get; set; }
         public Brand Brand{ get; set; }
+
         [Required]
         public int ColourId { get; set; }
         public Colour Colour { get; set; }
+
         [Required]
         public int ItemCategoryId { get; set; }
         public ItemCategory ItemCategory { get; set; }
+
         [Required]
         public int SizeId { get; set; }
         public Size Size { get; set; }
-        public int Price { get; set; }        
+
+        public int Price { get; set; }      
+        
         public int Qty { get; set; }
+
         public bool Active { get; set; }
+
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+
         public virtual IList<ProductPromotion> ProductPromotions { get; set; }
+
         public virtual IList<ProductSale> ProductSales { get; set; }
     }
 }
